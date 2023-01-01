f748ac6b3cde663c97100e112c80126804f23d0e
Eugen Paul <paul.eugen@gmx.de>
2023-01-01 10:33:19 +0100

    4. Median of Two Sorted Arrays - fast solution

--- 4MedianofTwoSortedArrays.py
+++ 4MedianofTwoSortedArrays.py
@@ -3,22 +3,36 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merge = list()
-        pos2 = 0
-        for i in nums1:
-            while pos2 < len(nums2) and nums2[pos2] < i:
-                merge.append(nums2[pos2])
-                pos2 += 1
-            merge.append(i)
+        full_length = len(nums1) + len(nums2)
+        half = full_length // 2
 
-        while pos2 < len(nums2):
-            merge.append(nums2[pos2])
-            pos2 += 1
+        A = nums1
+        B = nums2
 
-        if len(merge) % 2 == 0:
-            return (merge[len(merge) // 2 - 1] + merge[len(merge) // 2]) / 2
+        if len(A) > len(B):
+            A, B = B, A
 
-        return merge[len(merge) // 2]
+        left_1 = 0
+        right_1 = len(A) - 1
+
+        while True:
+            mid1 = (right_1 - left_1) // 2 + left_1
+            mid2 = half - mid1 - 2  # mid2 is gt -1, because A <= B
+
+            a_last = A[mid1] if mid1 >= 0 and mid1 < len(A) else -(10**7)
+            a_next = A[mid1+1] if mid1 >= -1 and mid1 < len(A)-1 else 10**7
+
+            b_last = B[mid2] if mid2 >= 0 and mid2 < len(B) else -(10**7)
+            b_next = B[mid2+1] if mid2 >= -1 and mid2 < len(B)-1 else 10**7
+
+            if a_last > b_next:
+                right_1 = mid1 - 1
+            elif b_last > a_next:
+                left_1 = mid1 + 1
+            elif full_length % 2 == 1:
+                return min(a_next, b_next)
+            else:
+                return (max(a_last, b_last) + min(a_next, b_next)) / 2
 
 
 if __name__ == "__main__":
