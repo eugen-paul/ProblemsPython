2575743035ae1df43278fa7ae9b62f4d26b445b6
Eugen Paul <paul.eugen@gmx.de>
2023-01-01 11:48:30 +0100

    5. Longest Palindromic Substring - expand

--- 5LongestPalindromicSubstring.py
+++ 5LongestPalindromicSubstring.py
@@ -16,6 +16,27 @@ class Solution:
                     longest = t
         return longest
 
+    def longest_palindrome_expand(self, s: str) -> str:
+        longest = s[0]
+
+        def expand(p1, p2):
+            if (p2 >= len(s)) or s[p1] != s[p2]:
+                return ""
+
+            while 0 < p1 <= p2 <= len(s)-2 and s[p1-1] == s[p2+1]:
+                p1 -= 1
+                p2 += 1
+
+            return s[p1:p2+1]
+
+        for i in range(len(s)-1):
+            poly1 = expand(i, i)
+            poly2 = expand(i, i+1)
+            best = poly1 if len(poly1) > len(poly2) else poly2
+            longest = best if len(best) > len(longest) else longest
+
+        return longest
+
 
 if __name__ == "__main__":
     s = Solution()
@@ -31,3 +52,23 @@ if __name__ == "__main__":
     t1 = s.longestPalindrome("cbbd")
     if t1 == "bb":
         print("OK 3")
+
+    t1 = s.longestPalindrome("a")
+    if t1 == "a":
+        print("OK 4")
+
+    t1 = s.longest_palindrome_expand("123456")
+    if t1 == "1":
+        print("OK 1")
+
+    t1 = s.longest_palindrome_expand("babad")
+    if t1 == "bab" or t1 == "aba":
+        print("OK 2")
+
+    t1 = s.longest_palindrome_expand("cbbd")
+    if t1 == "bb":
+        print("OK 3")
+
+    t1 = s.longest_palindrome_expand("a")
+    if t1 == "a":
+        print("OK 4")
