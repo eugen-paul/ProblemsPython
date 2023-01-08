f2bf2ce6a15cd40fbafdc6d4a05d15084f50e801
Eugen Paul <paul.eugen@gmx.de>
2023-01-08 16:06:42 +0100

    30. Substring with Concatenation of All Words

--- 30_SubstringWithConcatenationOfAllWords.py
+++ 30_SubstringWithConcatenationOfAllWords.py
@@ -1,8 +1,49 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        # own implementation of the official solution
+        word_len = len(words[0])
+
+        response = list()
+        rest_words_template = dict()
+        words_cnt = Counter(words)
+        for w in words:
+            rest_words_template[w] = rest_words_template.get(w, 0)+1
+
+        for start_offset in range(word_len):
+            pos = start_offset
+            window_cnt = Counter()
+            words_in_window = 0
+            while pos < len(s):
+                # read and add current word to word counter
+                current_word = s[pos:pos+word_len]
+                window_cnt[current_word] += 1
+
+                # check if new current word is OK
+                if rest_words_template.get(current_word, None) is None:
+                    # current word is not in words => clear window
+                    window_cnt.clear()
+                    words_in_window = 0
+                elif words_in_window == len(words) - 1:
+                    # window counter is big enough
+                    if words_cnt == window_cnt:
+                        # window counter is equal to words => add positon of first word to respone
+                        response.append(pos - (word_len * (len(words) - 1)))
+                    # remove first word from window to move window
+                    pos_first_word = pos - (word_len * (len(words) - 1))
+                    first_window_word = s[pos_first_word:pos_first_word+word_len]
+                    window_cnt[first_window_word] -= 1
+                else:
+                    words_in_window += 1
+
+                pos += word_len
+
+        return response
+
+    def findSubstring_3(self, s: str, words: List[str]) -> List[int]:
         word_len = len(words[0])
 
         response = list()
