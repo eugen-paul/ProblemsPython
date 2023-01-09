56c40ccf8afcc240a83caf72b6c04a064801ae31
Eugen Paul <paul.eugen@gmx.de>
2023-01-09 18:54:34 +0100

    33. Search in Rotated Sorted Array

--- 33_SearchInRotatedSortedArray.py
+++ 33_SearchInRotatedSortedArray.py
@@ -4,7 +4,30 @@ from typing import List
 class Solution:
 
     def search(self, nums: List[int], target: int) -> int:
-        return self.s(nums, target, 0, len(nums)-1)
+        left = 0
+        right = len(nums)-1
+
+        while True:
+            if left > right:
+                return -1
+
+            if left == right:
+                if nums[left] == target:
+                    return left
+                return -1
+
+            l_v = nums[left]
+            m_v = nums[(left+right)//2]
+            r_v = nums[right]
+
+            if l_v <= target <= m_v:
+                right = (left+right)//2
+            elif m_v+1 <= target <= r_v:
+                left = (left+right)//2+1
+            elif l_v <= m_v:
+                left = (left+right)//2+1
+            else:
+                right = (left+right)//2
 
     def s(self, nums: List[int], target: int, l: int, r: int) -> int:
         if l > r:
@@ -47,3 +70,4 @@ if __name__ == "__main__":
     do_test(0, [4, 5, 6, 7, 0, 1, 2], 0, 4)
     do_test(1, [4, 5, 6, 7, 0, 1, 2], 3, -1)
     do_test(3, [1], 0, -1)
+    do_test(4, [5, 1, 3], 5, 0)
