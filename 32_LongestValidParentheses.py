9795eec5c1a1b0c5f98e08313c475a8d30838dd2
Eugen Paul <paul.eugen@gmx.de>
2023-01-09 18:01:12 +0100

    32. Longest Valid Parentheses

---
+++ 32_LongestValidParentheses.py
@@ -0,0 +1,71 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        m = list()
+
+        lvl = 0
+        bst_lvl = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                lvl += 1
+                if lvl > len(m):
+                    m.append(i)
+            else:
+                if lvl >= 1:
+                    curr_len = i-m[lvl-1] + 1
+                    bst_lvl = max(bst_lvl, curr_len)
+                    if lvl < len(m):
+                        m.pop()
+                    lvl -= 1
+                else:
+                    lvl = 0
+                    m.clear()
+
+        return bst_lvl
+
+    def longestValidParentheses_fast_but_need_a_lot_of_memory(self, s: str) -> int:
+        m = dict()
+
+        lvl = 0
+        bst_lvl = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                lvl += 1
+                if lvl not in m:
+                    m[lvl] = i
+            else:
+                if lvl >= 1:
+                    curr_len = i-m[lvl] + 1
+                    bst_lvl = max(bst_lvl, curr_len)
+                    if (lvl+1) in m:
+                        m.pop(lvl+1)
+                    lvl -= 1
+                else:
+                    lvl = 0
+                    m.clear()
+
+        return bst_lvl
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.longestValidParentheses(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "(()", 2)
+    do_test(1, ")()())", 4)
+    do_test(2, "", 0)
+    do_test(3, ")(", 0)
+    do_test(4, "()", 2)
+    do_test(5, "())))))", 2)
+    do_test(6, "()()()", 6)
+    do_test(7, "()(())()", 8)
+    do_test(8, "((()))))))))))()()()()", 8)
+    do_test(9, "((()))))))))))()()", 6)
+    do_test(10, "((()))))))))", 6)
+    do_test(11, "()()()(", 6)
+    do_test(12, "((()))(()(", 6)
