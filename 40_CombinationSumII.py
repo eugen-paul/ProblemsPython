ad4ca9f3b52eeef647aeacd7a72c71e2f5c5dec7
Eugen Paul <paul.eugen@gmx.de>
2023-01-11 19:13:04 +0100

    40. Combination Sum II

--- 40_CombinationSumII.py
+++ 40_CombinationSumII.py
@@ -2,20 +2,24 @@ from typing import List
 
 
 class Solution:
-    def combinationSum_sub(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2_sub(self, candidates: List[int], target: int) -> List[List[int]]:
         if len(candidates) == 0 or candidates[0] > target:
             return []
 
         resp = []
 
+        last_number = -1
         for i, n in enumerate(candidates):
             rest = target - n
             if rest < 0:
                 break
+            if last_number == n:
+                continue
+            last_number = n
             if rest == 0:
                 resp.append([n])
                 return resp
-            sub_resp = self.combinationSum_sub(candidates[i:], rest)
+            sub_resp = self.combinationSum2_sub(candidates[i+1:], rest)
             for sub_list in sub_resp:
                 resp.append([n] + sub_list)
 
@@ -23,13 +27,13 @@ class Solution:
             return resp
         return []
 
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        return self.combinationSum_sub(sorted(candidates), target)
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        return self.combinationSum2_sub(sorted(candidates), target)
 
 
 def do_test(i: int, s, n, r):
     c = Solution()
-    resp = c.combinationSum(s, n)
+    resp = c.combinationSum2(s, n)
 
     resp = [sorted(x) for x in resp]
     r = [sorted(x) for x in r]
@@ -41,7 +45,18 @@ def do_test(i: int, s, n, r):
 
 
 if __name__ == "__main__":
-    do_test(0, [2, 3, 6, 7], 7, [[2, 2, 3], [7]])
-    do_test(1, [2, 3, 5], 8, [[2, 2, 2, 2], [2, 3, 3], [3, 5]])
-    do_test(2, [2], 1, [])
-    do_test(3, [8, 7, 4, 3], 11, [[8, 3], [7, 4], [4, 4, 3]])
+    do_test(0, [10, 1, 2, 7, 6, 1, 5], 8, [
+        [1, 1, 6],
+        [1, 2, 5],
+        [1, 7],
+        [2, 6]
+    ])
+
+    do_test(1, [2, 5, 2, 1, 2], 5, [
+        [1, 2, 2],
+        [5]
+    ])
+
+    do_test(2, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 3, [
+        [1, 1, 1]
+    ])
