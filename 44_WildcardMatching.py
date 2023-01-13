339ec530d3868310c26c7f1b63abb67500f87283
Eugen Paul <paul.eugen@gmx.de>
2023-01-13 19:43:19 +0100

    44. Wildcard Matching

--- 44_WildcardMatching.py
+++ 44_WildcardMatching.py
@@ -2,7 +2,58 @@ import re
 
 
 class Solution:
+    def is_equal(self, a: str, b: str) -> bool:
+        for i in range(len(a)):
+            if a[i] == "?" or b[i] == "?":
+                continue
+            if a[i] != b[i]:
+                return False
+
+        return True
+
+    def get_pos(self, s: str, p: str, offset: int) -> int:
+        if len(s) - offset < len(p):
+            return -1
+
+        for i in range(offset, len(s) - len(p) + 1):
+            a = s[i: i+len(p)]
+            if self.is_equal(a, p):
+                return i
+
+        return -1
+
     def isMatch(self, s: str, p: str) -> bool:
+        if len(p) == 0:
+            return len(s) == 0
+
+        while "**" in p:
+            p = p.replace("**", "*")
+
+        splits = p.split("*")
+
+        if len(splits) == 1:
+            return len(s) == len(p) and self.is_equal(s, p)
+
+        first = splits[0]
+        a = s[:len(first)]
+        if len(first) > 0 and not self.is_equal(a, first):
+            return False
+
+        last = splits[-1]
+        a = s[-len(last):]
+        if len(last) > 0 and not self.is_equal(a, last):
+            return False
+
+        offset = 0
+        for sp in splits:
+            next_pos = self.get_pos(s, sp, offset)
+            if next_pos == -1:
+                return False
+            offset = next_pos + len(sp)
+
+        return True
+
+    def isMatch_funny(self, s: str, p: str) -> bool:
         if len(p) == 0:
             return len(s) == 0
 
@@ -66,3 +117,13 @@ if __name__ == "__main__":
             "*b*ab*bb***abba*a**ab***b*aaa*a*b****a*b*bb**b**ab*ba**bb*bb*baab****bab*bbb**a*a*aab*b****b**ba**abba", False)
     do_test(6, "aabaabbbabbaaabbbbaabbbbbabbbbaabbaaaababaababbbabbbababbbbabbaaabaabbaaaaaabbababbaababbbaaaaaabbaaaabaabbbabaaababbaaabaabbababbbabaabbbababbbababaabbbbbaaaababbbbababbbaaabbabaabababaabbbbbaaaaabaabaab",
             "a***aba**a*a****bb**bb*aa**baa*b*b***a*baa***a*aba**ba***a**a*a*aa**abaab**a*ab*b*b*aaa*aa*b**a***b*ab", True)
+    do_test(7, "cb", "??", True)
+    do_test(8, "cb", "*", True)
+    do_test(9, "cb", "*a*", False)
+    do_test(10, "cb", "*a*", False)
+    do_test(11, "cb", "*c*", True)
+    do_test(12, "cb", "*b*", True)
+    do_test(13, "cb", "b*", False)
+    do_test(14, "cb", "*c", False)
+    do_test(15, "cb", "c*", True)
+    do_test(16, "cb", "*b", True)
