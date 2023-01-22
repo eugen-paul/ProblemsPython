eee7eb8ab297950ab1016eab6fda057ae1aecd5e
Eugen Paul <paul.eugen@gmx.de>
2023-01-22 09:52:22 +0100

    131. Palindrome Partitioning

--- Problems/131_PalindromePartitioning.py
+++ Problems/131_PalindromePartitioning.py
@@ -1,17 +1,39 @@
-from typing import List
+from typing import Dict, List
 
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        buf: Dict[str, List[List[str]]] = dict()
+
+        def get_subs(sub_s: str) -> List[List[str]]:
+            if len(sub_s) == 0:
+                return [[]]
+
+            if len(sub_s) == 1:
+                return [[sub_s]]
+
+            if sub_s in buf:
+                return buf[sub_s]
+
+            current_subs = list()
+
+            for i in range(len(sub_s)):
+                subsub = sub_s[:i+1]
+                if subsub == subsub[::-1]:
+                    current_subs.extend([[subsub] + x for x in get_subs(sub_s[i+1:])])
+
+            buf[sub_s] = current_subs
+
+            return current_subs
+
+        return get_subs(s)
+
+    def partition_slow(self, s: str) -> List[List[str]]:
         resp = list()
 
         current_pol = list()
 
         def get_subs(sub_s: str):
-            if len(sub_s) == 1:
-                resp.append(current_pol.copy() + [sub_s])
-                return
-
             if len(sub_s) == 0:
                 resp.append(current_pol.copy())
                 return
