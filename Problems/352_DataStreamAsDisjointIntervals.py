2866bb825c91e248083b1d3c985bd79ccd4d3927
Eugen Paul <paul.eugen@gmx.de>
2023-01-28 11:27:02 +0100

    352. Data Stream as Disjoint Intervals.py

--- Problems/352_DataStreamAsDisjointIntervals.py
+++ Problems/352_DataStreamAsDisjointIntervals.py
@@ -9,7 +9,27 @@ class SummaryRanges:
         self.data = list()
 
     def addNum(self, value: int) -> None:
+        """some ideas from solution"""
+        l = 0
+        r = len(self.data) - 1
+        while l <= r:
+            m = (l+r)//2
+            if self.data[m][0] <= value <= self.data[m][1]:
+                return
+            elif value < self.data[m][0]:
+                r = m-1
+            else:
+                l = m+1
+        pos = l
 
+        self.data.insert(pos, [value, value])
+        if pos < len(self.data)-1 and self.data[pos+1][0] - 1 == value:
+            self.data[pos: pos+2] = [[value, self.data[pos+1][1]]]
+
+        if pos > 0 and self.data[pos-1][1] + 1 == value:
+            self.data[pos-1: pos+1] = [[self.data[pos-1][0], self.data[pos][1]]]
+
+    def addNum_1(self, value: int) -> None:
         new_self: List[List[int]] = list()
         current = [value, value]
 
