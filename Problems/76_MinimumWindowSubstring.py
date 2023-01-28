8fe398dc4c28f2660ceee4647b0f4e865e4b521e
Eugen Paul <paul.eugen@gmx.de>
2023-01-28 15:24:59 +0100

    76. Minimum Window Substring

--- Problems/76_MinimumWindowSubstring.py
+++ Problems/76_MinimumWindowSubstring.py
@@ -3,6 +3,36 @@ from typing import Counter, List
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        """some ideas from solution"""
+        count_t = Counter(t)
+        size_ct = len(count_t)
+
+        l = 0
+        r = 0
+
+        resp = ""
+
+        while r < len(s):
+            if s[r] in count_t:
+                count_t[s[r]] -= 1
+                if count_t[s[r]] == 0:
+                    size_ct -= 1
+
+            while size_ct == 0:
+                if len(resp) == 0 or len(resp) > r-l+1:
+                    resp = s[l:r+1]
+
+                if s[l] in count_t:
+                    count_t[s[l]] += 1
+                    if count_t[s[l]] == 1:
+                        size_ct += 1
+                l += 1
+
+            r += 1
+
+        return resp
+
+    def minWindow_1(self, s: str, t: str) -> str:
         count_t = Counter(t)
 
         l = 0
