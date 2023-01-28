e3f77defbd6ceb4a4ffb35531e28911a2d4d0fb6
Eugen Paul <paul.eugen@gmx.de>
2023-01-28 10:59:12 +0100

    352. Data Stream as Disjoint Intervals.py

---
+++ Problems/352_DataStreamAsDisjointIntervals.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class SummaryRanges:
+
+    data: List[List[int]]
+
+    def __init__(self):
+        self.data = list()
+
+    def addNum(self, value: int) -> None:
+
+        new_self: List[List[int]] = list()
+        current = [value, value]
+
+        for interval in self.data:
+            if interval[1] < current[0]-1:
+                new_self.append(interval)
+            elif current[1] < interval[0] - 1:
+                new_self.append(current)
+                current = interval
+            else:
+                current[0] = min(current[0], interval[0])
+                current[1] = max(current[1], interval[1])
+
+        new_self.append(current)
+        self.data = new_self
+
+    def getIntervals(self) -> List[List[int]]:
+        return self.data
+
+
+def do_test(i: int, s, n, r):
+    c = SummaryRanges()
+    for i in range(1, len(n)):
+        if s[i] == "addNum":
+            c.addNum(n[i][0])
+        elif s[i] == "getIntervals":
+            resp = c.getIntervals()
+            if resp == r[i]:
+                print("OK", i)
+            else:
+                print("NOK", i, "expected", r[i], "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, ["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals"],
+            [[], [1], [], [3], [], [7], [], [2], [], [6], []],
+            [None, None, [[1, 1]], None, [[1, 1], [3, 3]], None, [[1, 1], [3, 3], [7, 7]], None, [[1, 3], [7, 7]], None, [[1, 3], [6, 7]]])
