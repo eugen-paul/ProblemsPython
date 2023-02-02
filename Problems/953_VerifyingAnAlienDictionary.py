c2af540743bb84741a8454ba6b0ffec6dda0f928
Eugen Paul <paul.eugen@gmx.de>
2023-02-02 17:57:51 +0100

    953. Verifying an Alien Dictionary

--- Problems/953_VerifyingAnAlienDictionary.py
+++ Problems/953_VerifyingAnAlienDictionary.py
@@ -3,6 +3,16 @@ from typing import List
 
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        m = {c: i for i, c in enumerate(order)}
+        repl = [[m[c] for c in w] for w in words]
+        return repl == sorted(repl)
+    
+    def isAlienSorted_internet(self, words: List[str], order: str) -> bool:
+        m = {c: i for i, c in enumerate(order)}
+        repl = [[m[c] for c in w] for w in words]
+        return all ( x <= y for (x,y) in zip(repl, repl[1:]))
+
+    def isAlienSorted_1(self, words: List[str], order: str) -> bool:
         repl = []
         for w in words:
             sub_w = [order.index(x) for x in w]
@@ -23,3 +33,4 @@ if __name__ == "__main__":
     do_test(0, ["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz", True)
     do_test(1, ["word", "world", "row"], "worldabcefghijkmnpqstuvxyz", False)
     do_test(2, ["apple", "app"], "abcdefghijklmnopqrstuvwxyz", False)
+    do_test(3, ["abc", "abc"], "abcdefghijklmnopqrstuvwxyz", True)
