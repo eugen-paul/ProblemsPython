e58ff16bc3710a4d30205cf9a20abfee06ea7f93
Eugen Paul <paul.eugen@gmx.de>
2023-02-02 19:38:03 +0100

    90. Subsets II

---
+++ Problems/90_SubsetsII.py
@@ -0,0 +1,61 @@
+from copy import deepcopy
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        resp = [[]]
+        last_sub = [[]]
+        last_val = -100
+
+        for s in nums:
+            if last_val != s:
+                last_sub = [r + [s] for r in resp]
+                resp.extend(deepcopy(last_sub))
+            else:
+                last_sub = [r + [s] for r in last_sub]
+                resp.extend(deepcopy(last_sub))
+            last_val = s
+
+        return resp
+
+    def subsetsWithDup_2(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        resp = [[]]
+
+        for s in nums:
+            resp += [row + [s] for row in resp]
+
+        return list(set(tuple(x) for x in resp))
+
+    def subsetsWithDup_1(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        resp = [[]]
+
+        for s in nums:
+            resp += [row + [s] for row in resp]
+
+        resp.sort()
+        l = resp[-1]
+        resp = [x for (x, y) in zip(resp, resp[1:]) if x != y]
+        if resp[-1] != l:
+            resp.append(l)
+        return resp
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.subsetsWithDup(s)
+    resp.sort()
+    r.sort()
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, [1, 2, 2], [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]])
+    do_test(1, [0], [[], [0]])
+    do_test(2, [4, 4, 4, 1, 4], [[], [1], [1, 4], [1, 4, 4], [1, 4, 4, 4], [1, 4, 4, 4, 4], [4], [4, 4], [4, 4, 4], [4, 4, 4, 4]])
