047429dc8ead65e201218bc7c642de1270372ed1
Eugen Paul <paul.eugen@gmx.de>
2023-02-05 11:30:23 +0100

    Some Tests

--- Problems/39_CombinationSum.py
+++ Problems/39_CombinationSum.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+
     def combinationSum_sub(self, candidates: List[int], target: int) -> List[List[int]]:
         if len(candidates) == 0 or candidates[0] > target:
             return []
@@ -26,6 +27,39 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         return self.combinationSum_sub(sorted(candidates), target)
 
+    def combinationSum_ways_2(self, target: int) -> int:
+        """ways to get target from [1,3,5]"""
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for sub_target in range(1, target+1):
+            dp[sub_target] = dp[sub_target - 1]
+
+            if sub_target >= 3:
+                dp[sub_target] += dp[sub_target - 3]
+            if sub_target >= 5:
+                dp[sub_target] += dp[sub_target - 5]
+
+        return dp[target]
+
+    def combinationSum_ways_1(self, target: int) -> int:
+        """ways to get target from [1,3,5]"""
+        dp = [None] * (target + 1)
+
+        def get_n(sub_target: int) -> int:
+            if sub_target < 0:
+                return 0
+            if sub_target == 0:
+                return 1
+
+            if dp[sub_target]:
+                return dp[sub_target]
+
+            dp[sub_target] = get_n(sub_target-1) + get_n(sub_target-3) + get_n(sub_target-5)
+            return dp[sub_target]
+
+        return get_n(target)
+
 
 def do_test(i: int, s, n, r):
     c = Solution()
@@ -41,6 +75,16 @@ def do_test(i: int, s, n, r):
 
 
 if __name__ == "__main__":
+    c = Solution()
+    print(c.combinationSum_ways_1(1), c.combinationSum_ways_2(1))
+    print(c.combinationSum_ways_1(2), c.combinationSum_ways_2(2))
+    print(c.combinationSum_ways_1(3), c.combinationSum_ways_2(3))
+    print(c.combinationSum_ways_1(4), c.combinationSum_ways_2(4))
+    print(c.combinationSum_ways_1(5), c.combinationSum_ways_2(5))
+    print(c.combinationSum_ways_1(6), c.combinationSum_ways_2(6))
+    print(c.combinationSum_ways_1(7), c.combinationSum_ways_2(7))
+    print(c.combinationSum_ways_1(8), c.combinationSum_ways_2(8))
+
     do_test(0, [2, 3, 6, 7], 7, [[2, 2, 3], [7]])
     do_test(1, [2, 3, 5], 8, [[2, 2, 2, 2], [2, 3, 3], [3, 5]])
     do_test(2, [2], 1, [])
