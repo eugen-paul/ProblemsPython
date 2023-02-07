dd44e352435e186600ae0bd1771c7c4fe942bbb5
Eugen Paul <paul.eugen@gmx.de>
2023-02-07 19:13:51 +0100

    100. Same Tree

---
+++ Problems/100_SameTree.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+def deserialize(string):
+    if string == '[]':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val)) for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def do_test(i: int, s, n, r):
+    c = Solution()
+    resp = c.isSameTree(deserialize(s), deserialize(n))
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "[1,2,3]", "[1,2,3]", True)
+    do_test(1, "[1,2]", "[1,null,2]", False)
+    do_test(2, "[1,2,1]", "[1,1,2]", False)
