8d34c0af28ea5d38808f65ee4ccabc1cad8dd262
Eugen Paul <paul.eugen@gmx.de>
2023-02-11 18:35:40 +0100

    104. Maximum Depth of Binary Tree

---
+++ Problems/104_MaximumDepthOfBinaryTree.py
@@ -0,0 +1,55 @@
+from typing import Deque, List, Optional
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# ----
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        nodes = Deque()
+        nodes.append((root, 1))
+        resp = 0
+        while nodes:
+            node, deep = nodes.pop()
+            if not node:
+                continue
+            resp = max(resp, deep)
+            nodes.append((node.left, deep+1))
+            nodes.append((node.right, deep+1))
+
+        return resp
+
+
+def deserialize(string):
+    if string == '[]':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val)) for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.maxDepth(deserialize(s))
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "[3,9,20,null,null,15,7]", 3)
+    do_test(1, "[1,null,2]", 2)
