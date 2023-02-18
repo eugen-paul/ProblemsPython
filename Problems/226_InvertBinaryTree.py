1feb54110d72b303451cc9a0a597705a1dc8c784
Eugen Paul <paul.eugen@gmx.de>
2023-02-18 13:13:24 +0100

    226. Invert Binary Tree

---
+++ Problems/226_InvertBinaryTree.py
@@ -0,0 +1,76 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# ----
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
+def deserialize(string):
+    if string == '[]':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val)) for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if p is None and q is None:
+        return True
+    if p is None or q is None or p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+
+def levelOrder(root: Optional[TreeNode]) -> List[List[int]]:
+    resp = list()
+
+    def travel(node: Optional[TreeNode], level: int):
+        if not node:
+            return
+
+        if len(resp) <= level:
+            resp.append(list())
+
+        travel(node.left, level + 1)
+        resp[level].append(node.val)
+        travel(node.right, level + 1)
+
+    travel(root, 0)
+
+    return resp
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.invertTree(deserialize(s))
+    if isSameTree(resp, deserialize(r)):
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", levelOrder(resp))
+
+
+if __name__ == "__main__":
+    do_test(0, "[4,2,7,1,3,6,9]", "[4,7,2,9,6,3,1]")
+    do_test(1, "[2,1,3]", "[2,3,1]")
