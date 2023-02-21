f938ec2ba6191e621cc6d5890f305cc2e9557e37
Eugen Paul <paul.eugen@gmx.de>
2023-02-21 18:15:18 +0100

    540. Single Element in a Sorted Array

--- Problems/540_SingleElementInASortedArray.py
+++ Problems/540_SingleElementInASortedArray.py
@@ -3,9 +3,6 @@ from typing import List, Dict, Tuple, Counter
 
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-
         l, r = 0, len(nums) - 1
 
         while l < r:
