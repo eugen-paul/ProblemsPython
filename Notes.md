192e38fec922685828341f59a5dc5b9148f2319d
Eugen Paul <paul.eugen@gmx.de>
2023-03-19 16:47:01 +0100

    149. Max Points on a Line (sample solution)

--- Notes.md
+++ Notes.md
@@ -2,3 +2,4 @@
 
  - 2444. Count Subarrays With Fixed Bounds - was too hard for me.
  - 382. Linked List Random Node - work off the sample solution.
+ - 149. Max Points on a Line - work off the sample solution.
--- Problems/0_999/100_199/149_MaxPointsOnALine.py
+++ Problems/0_999/100_199/149_MaxPointsOnALine.py
@@ -48,6 +48,20 @@ class Solution:
             resp = max(resp, len(v))
         return resp + 1
 
+    def maxPoints(self, points: List[List[int]]) -> int:
+        """Sample solution"""
+        n = len(points)
+        if n == 1:
+            return 1
+        result = 2
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(n):
+                if j != i:
+                    cnt[math.atan2(points[j][1] - points[i][1],
+                                   points[j][0] - points[i][0])] += 1
+            result = max(result, max(cnt.values()) + 1)
+        return result
 
 def do_test(i: int, s, r):
     c = Solution()
