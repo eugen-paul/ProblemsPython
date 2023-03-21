312e577992d8e1a5255d7e93b30e17964b9b864b
Eugen Paul <paul.eugen@gmx.de>
2023-03-21 18:19:33 +0100

    151. Reverse Words in a String

---
+++ Problems/0_999/100_199/151_ReverseWordsInAString.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+import re
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.strip().split()))
+
+    def reverseWords_2(self, s: str) -> str:
+        s = re.sub(r"\s+", " ", s)
+        s = s.strip().split(" ")
+        return " ".join(w for w in reversed(s))
+
+    def reverseWords_1(self, s: str) -> str:
+        s = s.strip().split(" ")
+        resp = ""
+        for w in reversed(s):
+            if len(w) > 0:
+                resp += " " + w
+        return resp[1:]
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.reverseWords(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "the sky is blue", "blue is sky the")
+    do_test(1, "  hello world  ", "world hello")
+    do_test(2, "a good   example", "example good a")
