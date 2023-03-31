d7445120bb13ee314705f9639f34eecc04fb6524
Eugen Paul <paul.eugen@gmx.de>
2023-03-31 19:26:57 +0200

    1444. Number of Ways of Cutting a Pizza

--- Problems/1000_1999/1400_1499/1444_NumberOfWaysOfCuttingAPizza.py
+++ Problems/1000_1999/1400_1499/1444_NumberOfWaysOfCuttingAPizza.py
@@ -8,6 +8,33 @@ from typing import Deque, List, Dict, Set, Tuple, Counter
 
 
 class Solution:
+    def ways_s(self, pizza: List[str], k: int) -> int:
+        """Sample solution"""
+        rows = len(pizza)
+        cols = len(pizza[0])
+        apples = [[0] * (cols + 1) for row in range(rows + 1)]
+        for row in range(rows - 1, -1, -1):
+            for col in range(cols - 1, -1, -1):
+                apples[row][col] = ((pizza[row][col] == 'A')
+                                    + apples[row + 1][col]
+                                    + apples[row][col + 1]
+                                    - apples[row + 1][col + 1])
+        dp = [[[0 for col in range(cols)] for row in range(rows)] for remain in range(k)]
+        dp[0] = [[int(apples[row][col] > 0) for col in range(cols)] for row in range(rows)]
+        mod = 1000000007
+        for remain in range(1, k):
+            for row in range(rows):
+                for col in range(cols):
+                    val = 0
+                    for next_row in range(row + 1, rows):
+                        if apples[row][col] - apples[next_row][col] > 0:
+                            val += dp[remain - 1][next_row][col]
+                    for next_col in range(col + 1, cols):
+                        if apples[row][col] - apples[row][next_col] > 0:
+                            val += dp[remain - 1][row][next_col]
+                    dp[remain][row][col] = val % mod
+        return dp[k - 1][0][0]
+
     def ways(self, pizza: List[str], k: int) -> int:
         @cache
         def count_apple(start_x: int, start_y: int, end_x: int, end_y) -> int:
