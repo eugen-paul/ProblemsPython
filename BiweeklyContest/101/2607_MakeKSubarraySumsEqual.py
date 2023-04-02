79be0ec48f070f83b597ce0f7959f4fa63deda9f
Eugen Paul <paul.eugen@gmx.de>
2023-04-02 09:56:45 +0200

    2607. Make K-Subarray Sums Equal

--- BiweeklyContest/101/2607_MakeKSubarraySumsEqual.py
+++ BiweeklyContest/101/2607_MakeKSubarraySumsEqual.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from functools import cache
-from math import inf
+from math import gcd, inf
 import math
 from typing import Deque, List, Dict, Set, Tuple, Counter
 
@@ -10,8 +10,66 @@ from typing import Deque, List, Dict, Set, Tuple, Counter
 
 class Solution:
     def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
-        # TODO
-        pass
+        """using help"""
+
+        def comp_diff(bucket: List[int]) -> int:
+            resp = 0
+            bucket.sort()
+            median = bucket[len(bucket) // 2]
+            for n in bucket:
+                resp += abs(median-n)
+            return resp
+
+        resp = 0
+        for i in range(len(arr)):
+            bucket = []
+            pos = i
+            while arr[pos] != 0:
+                bucket.append(arr[pos])
+                arr[pos] = 0
+                pos = (pos+k) % len(arr)
+            if bucket:
+                resp += comp_diff(bucket)
+
+        return resp
+
+    def makeSubKSumEqual_i2(self, arr: List[int], k: int) -> int:
+        """internet solution"""
+        g = gcd(len(arr), k)
+        resp = 0
+        for i in range(g):
+            bucket = []
+            for j in range(i, len(arr), g):
+                bucket.append(arr[j])
+            bucket.sort()
+            median = bucket[len(bucket) // 2]
+            for n in bucket:
+                resp += abs(median-n)
+
+        return resp
+
+    def makeSubKSumEqual_i(self, arr: List[int], k: int) -> int:
+        """internet solution"""
+        """https://leetcode.com/problems/make-k-subarray-sums-equal/solutions/3367765/explaining-like-you-are-five-years-old/"""
+
+        n = len(arr)
+
+        no_of_buckets = gcd(k, n)
+        buckets = defaultdict(list)
+
+        for ind, num in enumerate(arr):
+            buckets[ind % no_of_buckets].append(num)
+
+        result = 0
+        for bucket_number, bucket in buckets.items():
+            sorted_bucket = sorted(bucket)
+            m = len(sorted_bucket)
+            median = sorted_bucket[(m)//2]
+
+            for num in bucket:
+                result += abs(median - num)
+
+        return result
 
     def makeSubKSumEqual_1(self, arr: List[int], k: int) -> int:
         """!WRONG!"""
