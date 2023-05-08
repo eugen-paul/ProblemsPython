bc2d89df2cb606d60c459a6d8cf682afe8361525
Eugen Paul <paul.eugen@gmx.de>
2023-05-08 18:54:16 +0200

    1822D Super-Permutation

---
+++ Codeforces/Problems/1822D_SuperPermutation.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+
+cases = int(input())
+
+for _ in range(cases):
+    n = int(input())
+    if n == 1:
+        print(1)
+        continue
+    if n & 1 == 1:
+        print(-1)
+        continue
+    resp = [0] * n
+    down = True
+    for i in range(n):
+        if down:
+            down = False
+            resp[i] = n-i
+        else:
+            down = True
+            resp[i] = i
+
+    print(" ".join([str(x) for x in resp]))
