2cac7f6b63ae2a8a069248ca0420ee768241db08
Eugen Paul <paul.eugen@gmx.de>
2023-06-04 15:58:30 +0200

    Codeforces Round 869 (Div. 2) C

--- Codeforces/Problems/1818/C_AlmostIncreasingSubsequence.py
+++ Codeforces/Problems/1818/C_AlmostIncreasingSubsequence.py
@@ -21,7 +21,7 @@ def i_matrix_int(h: int) -> List[List[int]]: return [list(map(int, input().split
 def inv(x): return pow(x % MOD, MOD - 2, MOD)
 
 
-def solve():
+def solve_s():
     """sample solution"""
     n, q = i_array_int()
     a = i_array_int()
@@ -37,41 +37,68 @@ def solve():
 
         if l == r:
             print(1)
-        elif l+1 == r:
-            print(2)
         else:
             print(r-l+1 - dp[r-2] + dp[l-1])
 
 
-# def solve_f():
-#     """MEMORY_LIMIT_EXCEEDED"""
-#     n, q = i_array_int()
-#     a = i_array_int()
+def solve():
+    n, q = i_array_int()
+    a = i_array_int()
 
-#     dp = [[0] * n for _ in range(n)]
+    dp = [0] * (n+1)
 
-#     for i in range(n-1):
-#         dp[2][i] = 2
+    dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
 
-#     for l in range(3, n):
-#         for start in range(n-l+1):
-#             end = start + l - 1
-#             a1 = dp[l-1][start] + (0 if a[end-2] >= a[end-1] and a[end-1] >= a[end] else 1)
-#             a2 = dp[l-1][start+1] + (0 if a[start] >= a[start+1] and a[start+1] >= a[start+2] else 1)
-#             dp[l][start] = max(
-#                 a1,
-#                 a2
-#             )
+    for l in range(3, n+1):
+        end = l - 1
+        dp[l] = dp[l-1] + (0 if a[end-2] >= a[end-1] and a[end-1] >= a[end] else 1)
 
-#     for _ in range(q):
-#         l, r = i_array_int()
-#         if l == r:
-#             print(1)
-#             continue
+    for _ in range(q):
+        l, r = i_array_int()
+        if l == r:
+            print(1)
+            continue
 
-#         print(dp[r-l+1][l-1])
+        s = 0
+        if dp[l-1] == dp[l]:
+            s = 1
+        if dp[l] == dp[l+1]:
+            s += 1
+        print(dp[r] - dp[l-1] + s)
 
-testData = """9 8
+
+def solve_f():
+    """MEMORY_LIMIT_EXCEEDED"""
+    n, q = i_array_int()
+    a = i_array_int()
+
+    dp = [[0] * n for _ in range(n)]
+
+    for i in range(n-1):
+        dp[2][i] = 2
+
+    for l in range(3, n):
+        for start in range(n-l+1):
+            end = start + l - 1
+            a1 = dp[l-1][start] + (0 if a[end-2] >= a[end-1] and a[end-1] >= a[end] else 1)
+            a2 = dp[l-1][start+1] + (0 if a[start] >= a[start+1] and a[start+1] >= a[start+2] else 1)
+            dp[l][start] = max(
+                a1,
+                a2
+            )
+
+    for _ in range(q):
+        l, r = i_array_int()
+        if l == r:
+            print(1)
+            continue
+
+        print(dp[r-l+1][l-1])
+
+
+testData = """9 9
 1 2 4 3 3 5 6 2 1
 1 3
 1 4
@@ -81,18 +108,41 @@ testData = """9 8
 7 8
 1 8
 8 8
+4 6
 """.split("\n")
 # testData = """1 1
 # 100
 # 1 1
 # """.split("\n")
-# testData = list()
 # testData = """2 3
 # 100 101
 # 1 1
 # 1 2
 # 2 2
 # """.split("\n")
+# testData = """5 1
+# 1 1 1 1 1
+# 2 5
+# testData = """5 7
+# 1 1 0 1 1
+# 1 2
+# 2 3
+# 3 4
+# 4 5
+# 1 5
+# 2 5
+# 3 5
+# # # """.split("\n")
+testData = """6 7
+1 2 3 1 1 1 1
+1 2
+1 3
+1 4
+1 5
+2 5
+3 5
+4 6
+# # """.split("\n")
 # testData = list()
 testDataPos = 0
 
