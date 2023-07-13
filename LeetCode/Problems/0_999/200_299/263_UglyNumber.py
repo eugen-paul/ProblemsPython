25efd62b4cdae7100910ac00c95bef40d5298173
Eugen Paul <paul.eugen@gmx.de>
2023-07-13 17:56:49 +0200

    263. Ugly Number

---
+++ LeetCode/Problems/0_999/200_299/263_UglyNumber.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+        return n == 1
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.isUgly(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, 6, True)
+    do_test(1, 1, True)
+    do_test(2, 14, False)
+    do_test(3, 0, False)
+    do_test(4, -10, False)
