fab66fedd37af5afa08d4a6f22b03efe7cc957d6
Eugen Paul <paul.eugen@gmx.de>
2023-07-30 16:32:26 +0200

    Codeforces Round 887 (Div. 2) C

--- Codeforces/Problems/CodeforcesRound887D2/C_NtarsisSet.py
+++ Codeforces/Problems/CodeforcesRound887D2/C_NtarsisSet.py
@@ -23,6 +23,55 @@ def inv(x): return pow(x % MOD, MOD - 2, MOD)
 
 
 def solve():
+    """
+                    1                   2
+    1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 
+
+    1 X 3   5 6 7
+      1   X       3   5 6 7
+          1         X       3   5 6 7
+                    1         X       3   5 6 7
+
+    k=1:
+    x 1
+
+    k=2:
+      x   1
+    x x   1
+
+    k=3:
+          x         1
+    x     x       x 1
+    x x x x x x x x 1
+
+    k=4:
+                    x         1
+              x     x       x 1
+              x x x x x x x x 1
+    x x x x x x x x x x x x x 1
+    """
+    for _ in range(i_int()):
+        n, k = i_array_int()
+        a = i_array_int()
+
+        if (a[0] != 1):
+            print(1)
+            continue
+
+        pos = 1  # counter of are missing number we are looking for
+        x = 1    # min counter of "x" that will be added to response array each round
+        for _ in range(k):
+            for x in range(x, n+1):  # try all numbers from x to n to find next missing number in a
+                if x == n:           # there are no missing numbers more
+                    break
+                if a[x] > pos+x:     # we are looking for "pos" missing number
+                    break
+            pos += x
+
+        print(pos)
+
+
+def solve_s():
     """sample solution"""
     for _ in range(i_int()):
         n, k = i_array_int()
@@ -35,6 +84,49 @@ def solve():
             x += j
             k -= 1
         print(x)
+
+
+def solve_1():
+    """internet solution"""
+    for _ in range(i_int()):
+        n, k = i_array_int()
+        a = i_array_int()
+
+        lb = 1
+        ub = n*k+10
+        while ub-lb > 1:
+            mid = (lb+ub)//2
+            tmp = mid
+            for _ in range(k):
+                tmp -= bisect.bisect(a, tmp)
+                if tmp < 0:
+                    break
+            if tmp <= 0:
+                lb = mid
+            else:
+                ub = mid
+        print(ub)
+
+
+def solve_2():
+    """internet solution"""
+    t = int(input())
+    for _ in range(t):
+        _, k = i_array_int()
+        a = i_array_int()
+
+        if (a[0] != 1):
+            print(1)
+            continue
+
+        z = 1
+        dz = 1
+        for _ in range(k):
+            z += dz
+            while bisect.bisect(a, z) > dz:
+                dz += 1
+                z += 1
+        print(z)
 
 
 testData = """7
