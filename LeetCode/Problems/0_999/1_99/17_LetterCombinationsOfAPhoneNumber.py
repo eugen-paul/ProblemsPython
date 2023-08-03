fd424b8a7018f01d4e4c34a3a63738c674a4ac7e
Eugen Paul <paul.eugen@gmx.de>
2023-08-03 18:31:24 +0200

    17. Letter Combinations of a Phone Number

--- LeetCode/Problems/0_999/1_99/17_LetterCombinationsOfAPhoneNumber.py
+++ LeetCode/Problems/0_999/1_99/17_LetterCombinationsOfAPhoneNumber.py
@@ -4,6 +4,56 @@ from typing import List
 class Solution:
 
     def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        nTol = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+            "0": [" "],
+        }
+        
+        resp = [""]
+        for d in reversed(digits):
+            tmp = []
+            for c in nTol[d]:
+                tmp += [c + t for t in resp]
+            resp = tmp
+            
+        return resp
+        
+        
+    def letterCombinations_2(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        m = dict()
+        m["2"] = "abc"
+        m["3"] = "def"
+        m["4"] = "ghi"
+        m["5"] = "jkl"
+        m["6"] = "mno"
+        m["7"] = "pqrs"
+        m["8"] = "tuv"
+        m["9"] = "wxyz"
+        m["0"] = " "
+
+        def solve(s: str) -> List[str]:
+            if len(s) == 1:
+                return list(m[s[0]])
+            sub = solve(s[1:])
+            resp = []
+            for c in m[s[0]]:
+                for u in sub:
+                    resp.append(c + u)
+            return resp
+        return solve(digits)
+
+    def letterCombinations_1(self, digits: str) -> List[str]:
         nTol = [
             [" "],
             [],
