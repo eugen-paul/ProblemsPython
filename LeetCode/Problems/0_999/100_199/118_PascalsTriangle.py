5f55ba5d271f54b60fa3c78d6d69c827ed70ee99
Eugen Paul <paul.eugen@gmx.de>
2023-09-08 15:13:40 +0200

    118. Pascal's Triangle

--- LeetCode/Problems/0_999/100_199/118_PascalsTriangle.py
+++ LeetCode/Problems/0_999/100_199/118_PascalsTriangle.py
@@ -4,6 +4,16 @@ from typing import List, Dict, Tuple, Counter
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         resp = [[1]]
+        for r in range(1, numRows):
+            sub = [1]
+            for i in range(r-1):
+                sub.append(resp[r-1][i] + resp[r-1][i+1])
+            sub.append(1)
+            resp.append(sub)
+        return resp
+
+    def generate_2(self, numRows: int) -> List[List[int]]:
+        resp = [[1]]
         last_row = resp[0]
         for row in range(2, numRows + 1):
             r = [1]
