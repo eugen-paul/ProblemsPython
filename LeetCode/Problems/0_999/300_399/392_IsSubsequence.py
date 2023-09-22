7c863626751bf8903e10269d9fed4a7e894f1c08
Eugen Paul <paul.eugen@gmx.de>
2023-09-22 09:27:58 +0200

    392. Is Subsequence

---
+++ LeetCode/Problems/0_999/300_399/392_IsSubsequence.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        pos = 0
+        for c in t:
+            if c == s[pos]:
+                pos += 1
+            if pos == len(s):
+                return True
+        return False
+
+
+def do_test(i: int, s, n, r):
+    c = Solution()
+    resp = c.isSubsequence(s, n)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "abc",  "ahbgdc", True)
+    do_test(1, "axc",  "ahbgdc", False)
+    do_test(2, "",  "ahbgdc", True)
+    do_test(2, "",  "", True)
+
+
+def get_nb(x: int, y: int, maxX: int, maxY: int, minX: int = 0, minY: int = 0, dia: bool = False) -> List[Tuple[int, int]]:
+    resp = []
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    if dia:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    for dx, dy in directions:
+        xn, yn = x+dx, y+dy
+        if minX <= xn <= maxX and minY <= yn <= maxY:
+            resp.append((xn, yn))
+    return resp
