1842363d0165672df00daa045b269bef80fca1f6
Eugen Paul <paul.eugen@gmx.de>
2023-10-01 09:57:01 +0200

    557. Reverse Words in a String III

---
+++ LeetCode/Problems/0_999/500_599/557_ReverseWordsInAStringIII.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join([c[::-1] for c in s.split()])
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.reverseWords(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, "Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")
+    do_test(1, "God Ding", "doG gniD")
+
+
+def get_nb(x: int, y: int, maxX: int, maxY: int, minX: int = 0, minY: int = 0, dia: bool = False) -> List[Tuple[int, int]]:
+    resp = []
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    if dia:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    for dx, dy in directions:
+        xn, yn = x+dx, y+dy
+        if minX <= xn <= maxX and minY <= yn <= maxY:
+            resp.append((xn, yn))
+    return resp
