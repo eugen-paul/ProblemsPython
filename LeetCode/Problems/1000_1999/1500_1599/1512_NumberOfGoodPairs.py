66c607d3059094e628d0851f1776b31d128079b7
Eugen Paul <paul.eugen@gmx.de>
2023-10-03 16:17:26 +0200

    1512. Number of Good Pairs

---
+++ LeetCode/Problems/1000_1999/1500_1599/1512_NumberOfGoodPairs.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        ans = 0
+        for c in cnt.values():
+            ans += c * (c-1) // 2
+        return ans
+
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.numIdenticalPairs(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, [1, 2, 3, 1, 1, 3], 4)
+    do_test(1, [1, 1, 1, 1], 6)
+    do_test(2, [1, 2, 3], 0)
+
+
+def get_nb(x: int, y: int, maxX: int, maxY: int, minX: int = 0, minY: int = 0, dia: bool = False) -> List[Tuple[int, int]]:
+    resp = []
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    if dia:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    for dx, dy in directions:
+        xn, yn = x+dx, y+dy
+        if minX <= xn <= maxX and minY <= yn <= maxY:
+            resp.append((xn, yn))
+    return resp
