f3690fdf04fffd0c970614f1046bd6a342c85b7d
Eugen Paul <paul.eugen@gmx.de>
2023-10-06 17:57:39 +0200

    343. Integer Break

---
+++ LeetCode/Problems/0_999/300_399/343_IntegerBreak.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+from functools import cache
+from math import inf
+from typing import Deque, List, Dict, Set, Tuple, Counter
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        ans = 1
+        while n > 4:
+            ans *= 3
+            n -= 3
+        ans *= n
+        return ans
+
+    def integerBreak(self, n: int) -> int:
+        ans = (0, 0, 1, 2, 4, 6, 9, 12, 18, 27, 36, 54, 81, 108, 162, 243, 324, 486, 729, 972, 1458, 2187, 2916, 4374, 6561, 8748, 13122, 19683, 26244, 39366, 59049, 78732, 118098, 177147, 236196, 354294, 531441, 708588, 1062882, 1594323, 2125764, 3188646, 4782969, 6377292, 9565938, 14348907, 19131876, 28697814, 43046721, 57395628, 86093442, 129140163, 172186884, 258280326, 387420489, 516560652, 774840978, 1162261467, 1549681956
+               )
+        return ans[n]
+
+def do_test(i: int, s, r):
+    c = Solution()
+    resp = c.integerBreak(s)
+    if resp == r:
+        print("OK", i)
+    else:
+        print("NOK", i, "expected", r, "response", resp)
+
+
+if __name__ == "__main__":
+    do_test(0, 2, 1)
+    do_test(1, 10, 36)
+    do_test(2, 3, 2)
+    do_test(3, 4, 4)
+    do_test(4, 5, 6)
+
+
+# for i in range(2, 59):
+#     c = Solution()
+#     print(c.integerBreak(i))
+
+
+def get_nb(x: int, y: int, maxX: int, maxY: int, minX: int = 0, minY: int = 0, dia: bool = False) -> List[Tuple[int, int]]:
+    resp = []
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    if dia:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    for dx, dy in directions:
+        xn, yn = x+dx, y+dy
+        if minX <= xn <= maxX and minY <= yn <= maxY:
+            resp.append((xn, yn))
+    return resp
